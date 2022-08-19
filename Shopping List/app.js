09b026f097819f392211c7706cd553f1b1fddd3e
Gabriel Kok <kokgabriel@gmail.com>
2022-08-19 20:59:31 +0800

    split them up

--- Shopping List/app.js
+++ Shopping List/app.js
@@ -1,19 +1,10 @@
-// import { getSearches } from "./autocomplete.js";
+import { getDropdown, navigateDropdown } from "./autocomplete.js";
 
 //This is like the usual grocery list app
 //extra functions: checkbox to cross out?
 //Autocomplete
 //There is a autocomplete pretty good code by Coding Nepal(bookmarked)
-
 //the async keyword makes the function return a promise
-const getSearches = async (userInput) => {
-  const response = await fetch(
-    `https://api.frontendeval.com/fake/food/${userInput}`
-  );
-
-  const searches = await response.json();
-  return searches;
-};
 
 const input = document.querySelector(".input-text");
 const submitBtn = document.querySelector(".submit-btn");
@@ -33,117 +24,12 @@ window.addEventListener("DOMContentLoaded", setupItems);
 
 //!When user types in to the input field, getSearches and open the dropdown results
 input.addEventListener("keyup", (e) => {
-  if (![37, 38, 39, 40, 13].includes(e.keyCode)) {
-    let results = [];
-    //get inputVal from the field
-    let inputVal = e.target.value;
-    //**** Since we calling API, we don't need this block below//
-    //   if (inputVal.length > 0) {
-    //     results = searches.filter((item) => {
-    //       return item.toLowerCase().includes(inputVal.toLowerCase());
-    //     });
-    //   }
-    if (inputVal.length > 0) {
-      const lowerCase = inputVal.toLowerCase();
-      getSearches(lowerCase).then(function (value) {
-        results = value;
-        renderResults(results);
-      });
-    }
-  }
+  getDropdown(e);
 });
 //! User can use arrow keys to navigate dropdown results. Add an 'Active' class to the selected result. Enter will simulate a click.
 input.addEventListener("keydown", (e) => {
-  let results;
-  //if there is a dropdown, get the dropdown results
-  if (dropdown.classList.contains("show")) {
-    results = document.querySelectorAll(".dropdown-results");
-    // console.log("The results are: ");
-    // console.log(results);
-  }
-  //if down arrow key pressed
-  if (e.keyCode == 40) {
-    currentFocus++;
-    addActive(results);
-  }
-  //if up arrow key pressed
-  else if (e.keyCode == 38) {
-    currentFocus--;
-    addActive(results);
-  }
-  //if enter key is pressed
-  else if (e.keyCode == 13) {
-    e.preventDefault();
-
-    //if user has already selected and there is no more dropdown, submit the item
-    if (!dropdown.classList.contains("show")) {
-      submitBtn.click();
-    }
-    if (currentFocus > -1) {
-      //simulate a click on the "active" item
-      if (results) {
-        results[currentFocus].click();
-      }
-    }
-  }
+  navigateDropdown(e);
 });
-//this functions adds focus to each item when selected in the dropdown
-function addActive(results) {
-  if (!results) return false;
-  //start by removing all "active" class on all items
-  results.forEach((result) => {
-    result.classList.remove("active");
-  });
-  //allow user to cycle up or down
-  if (currentFocus >= results.length) {
-    currentFocus = 0;
-  }
-  if (currentFocus < 0) {
-    currentFocus = results.length - 1;
-  }
-
-  //add "active" class to the currentFocus
-  results[currentFocus].classList.add("active");
-  //   console.log("active");
-}
-
-//!------Render Autocomplete Results-------------------
-function renderResults(results) {
-  const dropdown = document.querySelector(".dropdown");
-  if (!results.length) {
-    dropdown.classList.remove("show");
-    return;
-  }
-  //for each item in the results array, map it to a HTML element and then plug it into the DOM (to show in dropdown)
-  //Similar to the createListItem, but instead of AppendChild we use "=".
-  //This is different because the dropdown constantly changes completely whenever user types
-  //But When we createListItem, we want each created items to stay
-  let content = results
-    .map((item) => {
-      return `<li class="dropdown-results">${item}</li>`;
-    })
-    .join("");
-
-  dropdown.classList.add("show");
-  //plug it back into the DOM
-  dropdown.innerHTML = `<ul>${content}<ul>`;
-  //*Add event listeners to the results
-  const items = document.querySelectorAll(".dropdown-results");
-  items.forEach((item) => {
-    item.addEventListener("click", () => {
-      selectItem(item.innerHTML);
-    });
-  });
-}
-
-//When user clicks the result
-function selectItem(item) {
-  input.value = item;
-  //close the dropdown
-  dropdown.classList.remove("show");
-  //put the focus on the input field, so you press enter then will submit the item
-  input.focus();
-}
 
 //**--------------------------------End of autocomplete functions--------------------------------
 
